fd0801f0bfe783c8ab85abec7f37476efdb033ef
임우섭 <wooseopim93@gmail.com>
2020-05-19 10:53:30 +0900

    [Create] New project -우섭

---
+++ AnimalCrossing/Front/animal/src/views/Community.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="community">
+    <h1>This is an Community page</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ AnimalCrossing/Front/animal/src/views/Info.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="info">
+    <h1>This is an Info page</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
