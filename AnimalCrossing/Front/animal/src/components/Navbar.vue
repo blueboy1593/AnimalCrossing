4d6781f2ef512b2aa68e3462c64ff764bef70e8e
임우섭 <wooseopim93@gmail.com>
2020-05-21 22:13:36 +0900

    Fix Navbar -우섭

--- AnimalCrossing/Front/animal/src/components/Navbar.vue
+++ AnimalCrossing/Front/animal/src/components/Navbar.vue
@@ -1,64 +1,40 @@
 <template>
   <div id="anav">
     <v-app id="inspire">
-        <v-card
-        class="mx-auto overflow-hidden"
-        height="400"
-        >
-        <v-app-bar
-            color="deep-purple"
-            dark
-        >
-            <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-    
-            <v-toolbar-title>Title</v-toolbar-title>
+      <v-card class="mx-auto overflow-hidden" height="400">
+        <v-app-bar color="deep-purple" dark>
+          <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+          <v-toolbar-title>Title</v-toolbar-title>
         </v-app-bar>
-    
-        <v-navigation-drawer
-            v-model="drawer"
-            absolute
-            temporary
-        >
-            <v-list
-            nav
-            dense
-            >
+
+        <v-navigation-drawer v-model="drawer" absolute temporary>
+          <v-list nav dense>
             <v-list-item-group
-                v-model="group"
-                active-class="deep-purple--text text--accent-4"
+              v-model="group"
+              active-class="deep-purple--text text--accent-4"
             >
-                <v-list-item>
+              <v-list-item>
                 <v-list-item-icon>
-                    <v-icon>mdi-home</v-icon>
+                  <v-icon>mdi-home</v-icon>
                 </v-list-item-icon>
                 <v-list-item-title>Home</v-list-item-title>
-                </v-list-item>
-    
-                <v-list-item>
+              </v-list-item>
+
+              <v-list-item>
                 <v-list-item-icon>
-                    <v-icon>mdi-account</v-icon>
+                  <v-icon>mdi-account</v-icon>
                 </v-list-item-icon>
                 <v-list-item-title>Account</v-list-item-title>
-                </v-list-item>
-    
+              </v-list-item>
             </v-list-item-group>
-            </v-list>
+          </v-list>
         </v-navigation-drawer>
-        </v-card>
+      </v-card>
     </v-app>
-    </div>
+  </div>
 </template>
 
-<script>
-export default {
-  el: '#anav',
-  vuetify: new Vuetify(),
-  data: () => ({
-    drawer: false,
-  }),
-}
-</script>
+<script></script>
 
-<style>
-
-</style>
+<style></style>
