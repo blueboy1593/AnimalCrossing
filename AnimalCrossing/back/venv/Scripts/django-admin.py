1453712f4cc4d859154b3add7d35922bdc66705c
min1378 <qwes123@naver.com>
2020-05-25 16:27:13 +0900

    remove back directory

--- AnimalCrossing/back/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\multicampus\desktop\pjt2f\s02p31a403\animalcrossing\back\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
