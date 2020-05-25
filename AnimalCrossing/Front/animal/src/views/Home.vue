d10145c07c519e8ddc0854be99d7272020ff93c0
김강현 <snb0303@naver.com>
2020-05-25 01:50:18 +0000

    0525

--- AnimalCrossing/Front/animal/src/views/Home.vue
+++ AnimalCrossing/Front/animal/src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="home">
-    <h1>동물의숲 홈 화면!</h1>
+    <div class="box box1" style="cursor:pointer">
+      <div class="box1Con">
+        <img class="box1IconImg" src="../assets/images/logo.png" alt="">
+        <div class="box1Text">
+          도감
+        </div>
+      </div>
+    </div>
+
+    <div class="box box2" style="cursor:pointer">무 계산기</div>
+    <div class="box box3" style="cursor:pointer">거래소</div>
+    <div class="box box4" style="cursor:pointer">게시판</div>
   </div>
 </template>
 
@@ -9,3 +20,4 @@ export default {
   name: "Home"
 };
 </script>
+<style scoped src="./Home.css" />
