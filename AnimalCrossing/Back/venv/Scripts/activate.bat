488e1b258faa2feb612fedd490dbf55202729a33
임우섭 <wooseopim93@gmail.com>
2020-06-01 09:28:47 +0900

    venv files

--- AnimalCrossing/Back/venv/Scripts/activate.bat
+++ AnimalCrossing/Back/venv/Scripts/activate.bat
@@ -8,11 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-<<<<<<< HEAD
-set VIRTUAL_ENV=C:\Users\multicampus\SSAFY\3rd_project\s02p31a403\AnimalCrossing\Back\venv
-=======
 set VIRTUAL_ENV=C:\Users\multicampus\ssafy\lastProject\AnimalCrossing\Back\venv
->>>>>>> infoNeighbor
 
 if not defined PROMPT set PROMPT=$P$G
 
--- AnimalCrossing/Back/venv/Scripts/django-admin.py
+++ AnimalCrossing/Back/venv/Scripts/django-admin.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-#!c:\users\multicampus\ssafy\3rd_project\s02p31a403\animalcrossing\back\venv\scripts\python.exe
-=======
 #!c:\users\multicampus\ssafy\lastproject\animalcrossing\back\venv\scripts\python.exe
->>>>>>> infoNeighbor
 from django.core import management
 
 if __name__ == "__main__":
