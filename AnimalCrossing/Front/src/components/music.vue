fe27bfaf63186e2d121f65f9d1fbe0fd97d5e886
임우섭 <wooseopim93@gmail.com>
2020-06-01 12:15:27 +0900

    Fix eslint music.vue

--- AnimalCrossing/Front/src/components/music.vue
+++ AnimalCrossing/Front/src/components/music.vue
@@ -1,54 +1,52 @@
 <template>
   <!-- <div class="player"> -->
-    <!-- <div class="album">
+  <!-- <div class="album">
         <img src="../../src/assets/images/navi.png" class="albumImg" alt="" />
       </div> -->
-    <!-- <audio src="../../src/assets/audios/navi.mp3" class="audioCon" loop autoplay></audio> -->
-    <audio controls class="player">
-      <source src="../../src/assets/audios/navi.mp3" type="audio/mpeg">
-    </audio>
+  <!-- <audio src="../../src/assets/audios/navi.mp3" class="audioCon" loop autoplay></audio> -->
+  <audio controls class="player">
+    <source src="../../src/assets/audios/navi.mp3" type="audio/mpeg" />
+  </audio>
   <!-- </div> -->
 </template>
 
-<script>
-</script>
+<script></script>
 
 <style scoped>
-  .player {
-    /* display: grid; */
-    /* grid-area: music; */
-    width: 16%;
-    height: 8%;
-    /* overflow: hidden; */
-    /* border-radius: 10px; */
-    position: fixed;
-    right: 16%;
-    z-index: 999;
-    top: 4%;
-    opacity: 0.5;
-    /* background-color: rgba(36, 116, 136, 0.356); */
-    /* border-radius: 10px; */
-  }
+.player {
+  /* display: grid; */
+  /* grid-area: music; */
+  width: 16%;
+  height: 8%;
+  /* overflow: hidden; */
+  /* border-radius: 10px; */
+  position: fixed;
+  right: 16%;
+  z-index: 999;
+  top: 4%;
+  opacity: 0.5;
+  /* background-color: rgba(36, 116, 136, 0.356); */
+  /* border-radius: 10px; */
+}
 
-  .player:focus {
-	  outline:none;
-  }
+.player:focus {
+  outline: none;
+}
 
-  .album {
-    height: 100%;
-    border-radius: 50%;
-  }
+.album {
+  height: 100%;
+  border-radius: 50%;
+}
 
-  .albumImg {
-    transform: scale(0.1);
-    transform-origin: 0 0;
-    border-radius: 50%;
-    margin-top: 5%;
-    margin-left: 3%;
-  }
+.albumImg {
+  transform: scale(0.1);
+  transform-origin: 0 0;
+  border-radius: 50%;
+  margin-top: 5%;
+  margin-left: 3%;
+}
 
-  .audioCon {
-    width: 100%;
-  }
-
+.audioCon {
+  width: 100%;
+}
 </style>
