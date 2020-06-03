09e386f943c4bc2e788eaaf95fc79a5fc48e134a
blueboy1593 <snb0303@naver.com>
2020-06-03 10:41:31 +0900

    로그인, 로그아웃 기능 완성

--- AnimalCrossing/Front/src/store/index.js
+++ AnimalCrossing/Front/src/store/index.js
@@ -4,8 +4,33 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    isSigned: false,
+    user: {
+      id: 0, // 사용자 아이디 저장
+      // hasWallet: false, // 지갑을 가지고 있는지 여부 조회
+      email: "",
+      password: ""
+    }
+  },
+  mutations: {
+    setIsSigned(state, isSigned) {
+      state.isSigned = isSigned;
+    },
+    setUserId(state, id) {
+      state.user.id = id;
+    },
+    // setUserHasWallet(state, hasWallet) {
+    //   state.user.hasWallet = hasWallet;
+    // },
+    logout(state) {
+      state.isSigned = false;
+      state.user.id = 0;
+      state.user.hasWallet = false;
+      localStorage.removeItem("user");
+      localStorage.removeItem("password");
+    }
+  },
   actions: {},
   modules: {}
 });
--- AnimalCrossing/Front/src/views/Info.vue
+++ AnimalCrossing/Front/src/views/Info.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="info">
-    <sequential-entrance fromLeft>
-      <InfoNav />
-    </sequential-entrance>
+    <!-- <sequential-entrance fromLeft> -->
+    <InfoNav />
+    <!-- </sequential-entrance> -->
     <router-view />
   </div>
 </template>
