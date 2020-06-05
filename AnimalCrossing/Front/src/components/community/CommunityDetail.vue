7cd114cf103dbbf9e191b500c37d62f50ae2b00a
blueboy1593 <snb0303@naver.com>
2020-06-05 11:11:35 +0900

    게시글 post - firebase storage 사용

--- AnimalCrossing/Front/src/components/community/CommunityDetail.vue
+++ AnimalCrossing/Front/src/components/community/CommunityDetail.vue
@@ -12,7 +12,7 @@
           </p>
         </v-col>
       </v-row>
-      <div v-if="this.image == null" class="detailimage">
+      <div v-if="this.image === null" class="detailimage">
         <img
           src="https://ichef.bbci.co.uk/news/976/cpsprodpb/CA15/production/_111633715_df2cb9e9-4f34-499d-a255-29abf37d36d0.jpg"
           class="detailimage"
@@ -69,7 +69,7 @@ export default {
 
 <style>
 .detailimage {
-  width: 70%;
+  width: 400px;
   align-items: center;
   display: block;
   margin: 0px auto;
--- AnimalCrossing/Front/src/components/community/CommunityWrite.vue
+++ AnimalCrossing/Front/src/components/community/CommunityWrite.vue
@@ -41,6 +41,7 @@
 
 <script>
 import { writeShows } from "../../api/show.js";
+import * as firebase from "firebase";
 
 export default {
   name: "write",
@@ -57,11 +58,33 @@ export default {
     onClickImageUpload() {
       this.$refs.imageInput.click();
     },
-    onChangeImages(e) {
+    async onChangeImages(e) {
       console.log(e.target.files);
       const file = e.target.files[0];
       this.article.imageUrl = URL.createObjectURL(file);
+      await firebase
+        .storage()
+        .ref()
+        .child(file.name)
+        .put(file)
+        .then(response => {
+          console.log(response);
+          console.log("firebase 업로드");
+        });
+      let image = "";
+      await firebase
+        .storage()
+        .ref()
+        .child(file.name)
+        .getDownloadURL()
+        .then(response => {
+          console.log(response);
+          console.log("firebase 받아오기");
+          image = response;
+        });
+      this.article.imageUrl = image;
     },
+
     async write() {
       const token = this.$store.state.token;
       const user = this.$store.state.user;
@@ -70,7 +93,7 @@ export default {
         image =
           "https://ichef.bbci.co.uk/news/976/cpsprodpb/CA15/production/_111633715_df2cb9e9-4f34-499d-a255-29abf37d36d0.jpg";
       } else {
-        image = this.imageUrl;
+        image = this.article.imageUrl;
       }
       console.log(image);
       const article = {
