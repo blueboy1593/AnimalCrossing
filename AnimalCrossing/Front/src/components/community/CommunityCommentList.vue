c94cdd95bd2b8c5964f883533bafb7a169d5471a
SeolYu <seolyu.90@gmail.com>
2020-06-05 10:58:32 +0900

    [Update] 게시판 댓글 - 설유

--- AnimalCrossing/Front/src/components/community/CommunityCommentList.vue
+++ AnimalCrossing/Front/src/components/community/CommunityCommentList.vue
@@ -21,21 +21,22 @@ export default {
 
 <style>
 .onecomment {
-  /* border: 1px solid blue; */
   background-color: rgba(30, 143, 255, 0.075);
   border-top-left-radius: 10px;
   border-top-right-radius: 10px;
-  /* border-radius: 10px; */
-  /* border-bottom: 1.2px solid rgba(30, 143, 255, 0.295); */
   border-bottom: 1.2px solid rgba(76, 180, 157, 0.295);
   margin-bottom: 0.3rem;
   height: 50px;
   line-height: 50px;
-  /* font-weight: bold; */
   display: grid;
   grid-template-columns: 1fr 3fr 1fr;
   grid-template-areas: 
     "user comment time";
+}
+
+.onecomment:hover {
+  opacity: 0.8;
+  font-weight: bold;
 }
 
 .user {
