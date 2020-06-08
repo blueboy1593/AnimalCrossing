318de89c4e3136948fa24280d90e0c5c16ab7690
min1378 <qwes123@naver.com>
2020-06-08 22:55:40 +0900

    delete icons

--- AnimalCrossing/Front/src/plugins/vuetify.js
+++ AnimalCrossing/Front/src/plugins/vuetify.js
@@ -1,10 +1,5 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
-import "@mdi/font/css/materialdesignicons.css";
 Vue.use(Vuetify);
 
-export default new Vuetify({
-  icons: {
-    iconfont: "mdi" // default - only for display purposes
-  }
-});
+export default new Vuetify({});
