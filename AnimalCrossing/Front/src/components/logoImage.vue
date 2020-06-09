debd3c45b1c06c3b3a704c5c0ca9c3f6f5aaad92
min1378 <qwes123@naver.com>
2020-06-09 09:35:31 +0900

    favicon update

--- AnimalCrossing/Front/src/components/logoImage.vue
+++ AnimalCrossing/Front/src/components/logoImage.vue
@@ -6,8 +6,7 @@
       </router-link>
     </div>
     <span>
-    <img class="mdmd" src="../assets/images/mdmd.png" alt="">
-    <!-- <img class="mdmd" src="../assets/images/mdmd1.png" alt=""> -->
+      <img class="mdmd" src="../assets/images/mdmd.png" alt="" />
     </span>
     <p class="pinkBubble">모두 함께 즐기는 모여봐요 동물의 숲</p>
   </div>
@@ -18,12 +17,11 @@
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Jua&display=swap");
 
-
 * {
   background-image: none;
   background-size: cover;
   z-index: 2;
-  font-family: 'Jua', sans-serif;
+  font-family: "Jua", sans-serif;
   color: #e9f8f6;
 }
 
@@ -54,36 +52,34 @@
   height: 40px;
 }
 
-
 .pinkBubble {
-    display: none;
-    position: absolute;
-    /* width: 50px; */
-    padding: 5px;
-    -webkit-border-radius: 8px;
-    -moz-border-radius: 8px;
-    border-radius: 8px;
-    /* background: rgba(233, 97, 154, 0.315); */
-    background: #0e433a8e;
-    font-size: 14px;
-    text-align: center;
-    margin-top: -100px;
-    margin-left: 130px;
-    /* margin-left: 10px; */
+  display: none;
+  position: absolute;
+  /* width: 50px; */
+  padding: 5px;
+  -webkit-border-radius: 8px;
+  -moz-border-radius: 8px;
+  border-radius: 8px;
+  /* background: rgba(233, 97, 154, 0.315); */
+  background: #0e433a8e;
+  font-size: 14px;
+  text-align: center;
+  margin-top: -100px;
+  margin-left: 130px;
+  /* margin-left: 10px; */
 }
 
 .pinkBubble:after {
-    position: absolute;
-    bottom: -100%;
-    right: 50%;
-    border: solid transparent;
-    border-bottom-color: rgba(233, 97, 154, 0.315);
-    border-width: 4px;
-    content: " ";
+  position: absolute;
+  bottom: -100%;
+  right: 50%;
+  border: solid transparent;
+  border-bottom-color: rgba(233, 97, 154, 0.315);
+  border-width: 4px;
+  content: " ";
 }
 
-span:hover+p.pinkBubble {
-    display: block;
+span:hover + p.pinkBubble {
+  display: block;
 }
-
 </style>
